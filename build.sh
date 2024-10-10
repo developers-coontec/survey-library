368a9a65baace8227c5ab4f333bde37c8d62950f
jungsoo827 <jungsoo827@gmail.com>
2024-10-10 23:31:46 +0900

    ESG-1866 updated with new npm registry

--- build.sh
+++ build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 #npm install -g npm-cli-login
-npm-cli-login -u jenkins -p jenkins1234\!1 -e developers@coontec.com -r https://maven.meback.ai/repository/npm-private/
+npm-cli-login -u jenkins -p jenkins1234\!1 -e developers@coontec.com -r https://maven.planesg.ai/repository/npm-private/
 
-#npm set //maven.meback.ai/repository/npm-private/:_authToken NpmToken.0b029231-1b5d-3fd6-9817-a01f575d1ea4
-#npm login --registry https://maven.meback.ai/repository/npm-private/
 
 rm -rf build
 
-npm install --registry https://maven.meback.ai/repository/npm-group/
+npm install --registry https://maven.planesg.ai/repository/npm-group/
 #npm install
 
 
@@ -24,12 +22,12 @@ npm run build_knockout_ui
 npm run build_react_ui
 #
 cd build/survey-knockout
-npm publish --registry https://maven.meback.ai/repository/npm-private/
+npm publish --registry https://maven.planesg.ai/repository/npm-private/
 cd ../survey-core
-npm publish --registry https://maven.meback.ai/repository/npm-private/
+npm publish --registry https://maven.planesg.ai/repository/npm-private/
 cd ../survey-knockout-ui
-npm publish --registry https://maven.meback.ai/repository/npm-private/
+npm publish --registry https://maven.planesg.ai/repository/npm-private/
 cd ../survey-react
-npm publish --registry https://maven.meback.ai/repository/npm-private/
+npm publish --registry https://maven.planesg.ai/repository/npm-private/
 cd ../survey-react-ui
-npm publish --registry https://maven.meback.ai/repository/npm-private/
+npm publish --registry https://maven.planesg.ai/repository/npm-private/
